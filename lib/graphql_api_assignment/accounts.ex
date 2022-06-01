4364ced057bb8180cd2d6153e4feb7e48b8ba3ac
Brandon Tripp <brandongtripp@gmail.com>
2022-05-31 21:25:28 -0600

    Adding filtering by preferences and by first, before, and after

--- lib/graphql_api_assignment/accounts.ex
+++ lib/graphql_api_assignment/accounts.ex
@@ -1,22 +1,38 @@
 defmodule GraphqlApiAssignmentWeb.Accounts do
   alias GraphqlApiAssignment.Accounts.User
   alias GraphqlApiAssignment.Accounts.Preference
+  alias GraphqlApiAssignment.Repo
   alias EctoShorts.Actions
 
+  import Ecto.Query
+
   def all(params \\ %{}) do
-    params = do_all_build_params(params)
-    Actions.all(User, params)
+    %{filters: filter_params, preference: preference_params}= do_all_build_params(params)
+
+    filter_params = Keyword.new(filter_params)
+    preference_params = Keyword.new(preference_params)
+    
+    User 
+    |> join(:inner, [u], p in assoc(u, :preference), as: :preference)
+    |> where_preference(preference_params)
+    |> Actions.all(filter_params)
+  end
+
+  defp where_preference(query, preference) do 
+    Enum.reduce(preference, query, fn {key, val}, acc -> 
+      where(acc, [preference: p], field(p, ^key) == ^val)
+    end)
   end
 
   defp do_all_build_params(params) do 
-    params = Enum.reduce(params, %{preference: %{}}, fn filter, acc -> 
+    params = Enum.reduce(params, %{preference: %{}, filters: %{}}, fn filter, acc -> 
       {key, value} = filter
       regex = ~r/likes_[a-z_]/
       cond do 
         String.match?(Atom.to_string(key), regex) -> 
           acc = put_in(acc[:preference][key], value)
         true -> 
-          acc = put_in(acc[key], value)
+          acc = put_in(acc[:filters][key], value)
       end
     end)
   end
