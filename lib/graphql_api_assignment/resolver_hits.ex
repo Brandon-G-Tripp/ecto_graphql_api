0a4888dfad897f6153daa0724d6e4cf1ca9fb078
Brandon Tripp <brandongtripp@gmail.com>
2022-06-24 17:39:53 -0600

    clean up IO.inspects

--- lib/graphql_api_assignment/resolver_hits.ex
+++ lib/graphql_api_assignment/resolver_hits.ex
@@ -2,7 +2,6 @@ defmodule GraphqlApiAssignment.ResolverHits do
   alias GraphqlApiAssignment.ResolverHitGenServer
 
   def find(key) do 
-    IO.inspect(key, label: "key")
     value = ResolverHitGenServer.get_all_hits(key) 
       
     {:ok, value}
