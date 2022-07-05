e0d6e9d5ff5de29de05d6c4cecab43bf5d527a0f
Brandon Tripp <brandongtripp@gmail.com>
2022-07-05 11:53:10 -0600

    update of module name for tracking of hits to resolvers

--- lib/graphql_api_assignment_web/resolvers/user.ex
+++ lib/graphql_api_assignment_web/resolvers/user.ex
@@ -1,34 +1,33 @@
 defmodule GraphqlApiAssignmentWeb.Resolvers.User do
   alias GraphqlApiAssignment.Accounts
-  alias GraphqlApiAssignment.ResolverHitGenServer
+  alias GraphqlApiAssignment.ResolverHits
 
   def find(%{id: id}, _) do
-    ResolverHitGenServer.add_hit("user")
+    ResolverHits.add_hit("user")
     id = String.to_integer(id)
     Accounts.find(%{id: id})
   end
 
   def all(params, _) do
-    ResolverHitGenServer.add_hit("users")
+    ResolverHits.add_hit("users")
     users = Accounts.all(params)
     {:ok, users}
   end
 
   def create_user(params, _) do
-    ResolverHitGenServer.add_hit("create_user")
+    ResolverHits.add_hit("create_user")
     Accounts.create_user(params)
   end
 
   def update_user(%{id: id} = params, _) do
-   ResolverHitGenServer.add_hit("update_user")
+    ResolverHits.add_hit("update_user")
     id = String.to_integer(id)
     Accounts.update_user(id, Map.delete(params, :id))
   end
 
   def update_user_preference(%{user_id: id} = params, _) do
-    ResolverHitGenServer.add_hit("update_user_preference")
+    ResolverHits.add_hit("update_user_preference")
     id = String.to_integer(id)
     Accounts.update_user_preference(id, Map.delete(params, :user_id))
   end
-
 end
