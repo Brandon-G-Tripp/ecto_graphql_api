406ee0eb458fb2a20390ff3c00ff929bd5ef3820
Brandon Tripp <brandongtripp@gmail.com>
2022-07-05 11:52:08 -0600

    change from GenServer to Agent for basic state retrieval and update

--- lib/graphql_api_assignment/resolver_hits.ex
+++ lib/graphql_api_assignment/resolver_hits.ex
@@ -1,8 +1,32 @@
 defmodule GraphqlApiAssignment.ResolverHits do 
-  alias GraphqlApiAssignment.ResolverHitGenServer
+  use Agent
+  alias GraphqlApiAssignment.ResolverHits
+
+  @default_name ResolverHits
+
+  #API
+  
+  def start_link(opts \\ []) do 
+    opts = Keyword.put_new(opts, :name, @default_name)
+
+    Agent.start_link(fn -> %{} end, opts)
+  end
+
+  def add_hit(query_name, name \\ @default_name ) do 
+    Agent.update(name, fn state -> 
+      Map.update(state,query_name, 1, &(&1 + 1))
+    end)
+  end
+
+  def get(key, name \\ @default_name) do 
+    Agent.get(name, fn state -> 
+      val = Map.get(state, key, 0)
+      %{key: key, query_hits: val}
+    end)
+  end
 
   def find(key) do 
-    value = ResolverHitGenServer.get_all_hits(key) 
+    value = ResolverHits.get(key) 
       
     {:ok, value}
   end
